d45a2dbbde780459c70dbfa633145e82e5f7c0c0
raghav <raghav1987@gmail.com>
2014-05-30 17:18:55 -0400

    some stuff that i need to build over

---
+++ script.rb
@@ -0,0 +1,24 @@
+require 'json'
+require 'net/http'
+require 'uri'
+require 'geocoder'
+
+
+# response = Net::HTTP.get_response("http://api.citybik.es", "/citi-bike-nyc.json")
+# p response.body
+
+API_KEY = "AIzaSyBB2uDlsQn6B_b-ru0vaMtnYdg1QVPYwmg"
+
+uri = URI.parse("http://www.citibikenyc.com/stations/json")
+res = Net::HTTP.get_response(uri)
+puts res.body
+
+
+
+location = Geocoder.search('48 Wall Street, New York, NY')
+p location[0].latitude
+p location[0].longitude
+
+# response = Net::HTTP.post_form(uri, {"search" => "Berlin"})
+# p response.methods
+# p response.body
