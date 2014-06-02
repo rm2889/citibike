78338696dbe66f1da048097316370173d93ddeff
raghav <raghav1987@gmail.com>
2014-06-01 21:31:29 -0400

    able to get all data and parse through the JSON

--- Gemfile
+++ Gemfile
@@ -2,3 +2,6 @@ source 'https://rubygems.org'
 
 gem 'google-api-client'
 gem 'geocoder'
+gem 'json'
+gem 'net/http'
+gem 'uri'
--- script.rb
+++ script.rb
@@ -7,7 +7,7 @@ require 'geocoder'
 # response = Net::HTTP.get_response("http://api.citybik.es", "/citi-bike-nyc.json")
 # p response.body
 
-API_KEY = "AIzaSyBB2uDlsQn6B_b-ru0vaMtnYdg1QVPYwmg"
+DIRECTIONS_API_KEY = "AIzaSyBB2uDlsQn6B_b-ru0vaMtnYdg1QVPYwmg"
 
 uri = URI.parse("http://www.citibikenyc.com/stations/json")
 res = Net::HTTP.get_response(uri)
@@ -22,3 +22,10 @@ p location[0].longitude
 # response = Net::HTTP.post_form(uri, {"search" => "Berlin"})
 # p response.methods
 # p response.body
+
+=begin
+
+create a bike class, which would have all this relevant info, and a city class, which
+would have a list of bike stations
+
+=end
